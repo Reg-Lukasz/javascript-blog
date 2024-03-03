34a753a08387330727a1e67b5cd75c5b714c38ca
Lukasz Reglinski <lukasz.reglinski@gmail.com>
2024-03-03 18:39:58 +0100

    Add code in in script.js to find and show correct article by article link

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log('clickedElement:', clickedElement);
   // console.log(event);
 
   /* [DONE] remove class 'active' from all article links  */
-
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
@@ -15,21 +15,25 @@ function titleClickHandler(event){
   }
 
   /* [DONE] add class 'active' to the clicked link */
-
   clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
-
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  /* get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('Article id:', articleSelector);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('Article selected:', targetArticle);
+
+  /* [DONE] add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
